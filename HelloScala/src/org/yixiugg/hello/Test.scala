ab603466a0f582367f46fe93406ef793c78a5037
yixiugg <yjiang@statestreet.com>
2012-02-12 15:29:30 +0800

    init import

---
+++ HelloScala/src/org/yixiugg/hello/Test.scala
@@ -0,0 +1,12 @@
+package org.yixiugg.hello
+
+object Test {
+  
+  def main (args:Array[String]){
+//    args.foreach(println)
+    var arr=(1,"HI",2)
+     println(arr._1)
+      println(arr._2)
+       println(arr._3)
+  }
+}
